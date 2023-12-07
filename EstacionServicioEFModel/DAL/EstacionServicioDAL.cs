1fb2aea8dd1518224598cb11ac18617bd235dd4f
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-07 10:47:16 -0300

    Ok

---
+++ EstacionServicioEFModel/DAL/EstacionServicioDAL.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionServicioEFModel.DAL
+{
+    public class EstacionServicioDAL
+    {
+        dbestacionservicioefwebEntities db = new dbestacionservicioefwebEntities();
+
+        public void Save(EstacionServicio es)
+        {
+            db.EstacionServicio.Add(es);
+            db.SaveChanges();
+        }
+
+        public List<EstacionServicio> GetAll()
+        {
+            return db.EstacionServicio.ToList();
+        }
+
+        public List<EstacionServicio> GetAll(int capacidadMinima)
+        {
+            List<EstacionServicio> estacionesServicio = db.EstacionServicio.ToList();
+            if (capacidadMinima == 30)
+            {
+                return estacionesServicio.FindAll(es => es.CapacidadMaxima <= 39);
+            }
+            else
+            {
+                return estacionesServicio.FindAll(es => es.CapacidadMaxima > 39);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            EstacionServicio es = db.EstacionServicio.Find(id);
+            db.EstacionServicio.Remove(es);
+            db.SaveChanges();
+        }
+
+        public EstacionServicio Find(int id)
+        {
+            List<EstacionServicio> estacionesServicio = db.EstacionServicio.ToList();
+            EstacionServicio estacionServicio = estacionesServicio.Find(es => es.Id == id);
+            return estacionServicio;
+        }
+
+        public void Update(EstacionServicio es)
+        {
+            EstacionServicio estacionServicioBorrar = db.EstacionServicio.Find(es.Id);
+            db.EstacionServicio.Remove(estacionServicioBorrar);
+            db.EstacionServicio.Add(es);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ EstacionServicioEFModel/DAL/PuntoCargaDAL.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionServicioEFModel.DAL
+{
+    public class PuntoCargaDAL
+    {
+        dbestacionservicioefwebEntities db = new dbestacionservicioefwebEntities();
+
+        public void Save(PuntoCarga pc)
+        {
+            db.PuntoCarga.Add(pc);
+            db.SaveChanges();
+        }
+
+        public List<PuntoCarga> GetAll()
+        {
+            return db.PuntoCarga.ToList();
+        }
+
+        public List<PuntoCarga> GetAll(int capacidadMinima)
+        {
+            List<PuntoCarga> puntosCarga = db.PuntoCarga.ToList();
+            if (capacidadMinima == 1)
+            {
+                return puntosCarga.FindAll(es => es.CapacidadMaxima <= 3);
+            }
+            else
+            {
+                return puntosCarga.FindAll(es => es.CapacidadMaxima > 3);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            PuntoCarga pc = db.PuntoCarga.Find(id);
+            db.PuntoCarga.Remove(pc);
+            db.SaveChanges();
+        }
+
+        public PuntoCarga Find(int id)
+        {
+            return db.PuntoCarga.Find(id);
+        }
+
+        public void Update(PuntoCarga pc)
+        {
+            PuntoCarga puntoCargaBorrar = db.PuntoCarga.Find(pc.Id);
+            db.PuntoCarga.Remove(puntoCargaBorrar);
+            db.PuntoCarga.Add(pc);
+            db.SaveChanges();
+        }
+    }
+}
